acf15a72c1804c7c35b44d6f2155910ce352fd45
Deepa-Dhanashekar <88785401+Deepa-Dhanashekar@users.noreply.github.com>
2024-07-11 15:31:13 +0530

    Update StringCalculator.h trial

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,1 +1,16 @@
+#ifndef STRINGCALCULATOR_H
+#define STRINGCALCULATOR_H
+
+#include <string>
+
+class StringCalculator {
+public:
+    int add(const std::string& input);
+    int addofnum(const std::string& input);
+    void verify_NegativeNum(const std::string& input);
+    std::string normalizeDelimiters(const std::string& input);
+    std::string newlinecheck(const std::string& input);
+};
+
+#endif
 
