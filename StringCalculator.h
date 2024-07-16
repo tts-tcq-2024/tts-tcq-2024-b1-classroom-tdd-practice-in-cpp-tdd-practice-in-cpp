573dcf2704f0e72ab15048b22e3f96470d44d452
Sahil Wankhade <110714300+ArcheR1901@users.noreply.github.com>
2024-07-16 17:11:48 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,1 +1,21 @@
+#ifndef STRING_CALCULATOR_H
+#define STRING_CALCULATOR_H
+
+#include <string>
+#include <vector>
+#include <stdexcept>
+
+class StringCalculator {
+public:
+    int add(const std::string& numbers);
+
+private:
+    bool isCustomDelimiter(const std::string& numbers);
+    void parseCustomDelimiter(const std::string& numbers, std::string& delimiters, std::string& numbersWithoutDelimiters);
+    std::vector<int> split(const std::string& str, const std::string& delimiters);
+    int calculateSum(const std::vector<int>& nums);
+    void throwNegativeNumbersException(const std::vector<int>& negativeNumbers);
+};
+
+#endif // STRING_CALCULATOR_H
 
