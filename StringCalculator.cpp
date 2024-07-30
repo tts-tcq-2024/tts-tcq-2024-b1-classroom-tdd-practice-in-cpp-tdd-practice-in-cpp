ecc89b18b2cfbd8b118bb1679a55de1889837bbe
Sahil Wankhade <110714300+ArcheR1901@users.noreply.github.com>
2024-07-30 10:55:38 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,8 +1,7 @@
 #include "StringCalculator.h"
 #include <sstream>
 #include <algorithm>
- 
- 
+
 int StringCalculator::addofnum(const std::string& input) {
     std::istringstream stream(input);
     std::string number;
@@ -15,17 +14,15 @@ int StringCalculator::addofnum(const std::string& input) {
     }
     return sum;
 }
- 
 void StringCalculator::verify_NegativeNum(const std::string& input) {
     std::istringstream stream(input);
     std::string number;
     while (std::getline(stream, number, ',')) {
         if (std::stoi(number) < 0) {
-            throw std::runtime_error("Negative numbers not allowed");
+            throw std::runtime_error("Negative numbers is not allowed");
         }
     }
 }
- 
 int StringCalculator::add(const std::string& input) {
   if (input.empty()) {
         return 0;
@@ -33,19 +30,16 @@ int StringCalculator::add(const std::string& input) {
    if (input == "0") {
         return 0;
     }
- 
   std::string filteredinput = newlinecheck(input);
   filteredinput = normalizeDelimiters(filteredinput);
   verify_NegativeNum(filteredinput);
   return addofnum(filteredinput);  
 }
- 
 std::string StringCalculator::normalizeDelimiters(const std::string& input) {
     std::string result = input;
     std::replace(result.begin(), result.end(), '\n', ',');
     return result;
 }
- 
 std::string StringCalculator::newlinecheck(const std::string& input) {
     if (input.substr(0, 2) == "//") {
         std::string delimiter = input.substr(2, input.find('\n') - 2);
